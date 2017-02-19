6d91ee4ebadc9e1625c7b8d06bbd2b67fc4fa2f0
Felipe Carvalho <felipe_fsc@hotmail.com>
2017-02-19 01:09:20 -0300

    Reformulando projeto

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,51 @@
-var path = require('path');
-var webpack = require('webpack');
-
-var config = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './app/main'
-  ],
-  output: {
-    path: path.join(__dirname, 'public', 'js'),
-    filename: 'bundle.js',
-    publicPath: '/js'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          plugins: [
-            ['react-transform', {
-              transforms: [
-                {
-                  transform: 'react-transform-hmr',
-                  imports: ['react'],
-                  locals: ['module']
-                }, {
-                  transform: 'react-transform-catch-errors',
-                  imports: ['react', 'redbox-react']
-                }
-              ]
-            }]
-          ]
-        }
-      }
-    ]
-  }
-};
-
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        screw_ie8: true,
-        warnings: false
-      }
-    })
-  );
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        path: './public',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        contentBase: './public',
+        port: 3333,
+        historyApiFallback: true
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        },
+        {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        },
+        {
+            test: /\.png$/,
+            loader: 'url-loader?limit=100000'
+        },
+        {
+            test: /\.jpg$/,
+            loader: 'file-loader'
+        },
+        {
+            test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=application/font-woff'
+        },
+        {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=application/octet-stream'
+        },
+        {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'file'
+        },
+        {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=image/svg+xml'
+        }]
+    }
 }
-
-module.exports = config;
